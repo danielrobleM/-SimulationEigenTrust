f92aa785057afd1e7bec3001c683ae06ed465178
MemoLabra <gmlabra@gmail.com>
2013-09-26 00:24:13 -0400

    Update TrabajoEigen.py
    Actualizado con Caso 1 ( Basico )
    Revisar codigo de iteraciones
    revisar caso de la variable a

--- TrabajoEigen.py
+++ TrabajoEigen.py
@@ -74,8 +74,80 @@ if(TipoDeSimulacion==0):
     print 'Vector t2 final', t2.sum(axis=0)
 
 if (TipoDeSimulacion==1):
-    print 'Aca va el codigo 2'
 
+    #Numero Total de peers a simular
+    
+    Dimension=int(raw_input("Ingresar numero de peers: "))
+    a=float(raw_input("Nivel de confianza para Peers Confiables: "))
+    
+    #Generando Matriz Sij
+    #B=np.matrix('0. 3 1 0; 1 0 4 7; 2 -1 0 -1 ;-5 4 3 0')
+    B=np.random.randint(-Dimension,Dimension, size=(Dimension, Dimension))
+    
+    #Haciendo diagonal de la matriz 0
+    for i in range(len(B)):
+        for j in range(len(B)):
+            if (i==j):
+                B[i,j]=0
+         #print B
+    #A = matrix( [[0,3,1,0],[1,0,4,7],[2,-1,0,-1],[-5,4,3,0]])
+    
+    MatrixNormal=np.identity(Dimension)
+    #print 'Matriz Sij ='
+    #print MatrixNormal
+    print 'Matriz de Si,j'
+    print B
+    
+    #Generando el Vecto p<----------
+    p= np.empty(Dimension)
+    matrix(p.fill(1/Dimension))
+
+    print 'Vecto de Confianza p =',p
+
+    #Buscando filas que suman 0 y cambiandolas por vector de pre confianza
+    for i in range(len(B)):
+        for j in range(len(B)):
+            C=B.sum(axis=1) # suma de la fila
+            if C[i]==0:
+            	#print ' la suma de la fila es 0 cambio por vector de pre confianza'
+                B[i]=p
+                print B
+
+    # Normalizing Local Trust Values
+    
+    for i in range(len(B)):
+        for j in range(len(B)):
+            C=B.sum(axis=1) # suma de la fila
+       		#print C
+            SumaDeLaFila=int(C[i])
+       		#print 'la fila suma ',SumaDeLaFila
+            value=int(B[i,j])
+            valueForNormalMatrix=value/SumaDeLaFila
+            MatrixNormal[i,j]= float(valueForNormalMatrix)
+
+
+    print "Matrix Normal "
+    print MatrixNormal
+
+    #Comienza iteracion
+    Delta=1
+    t=p.T
+    print 'Vector Trapuesto t',t
+    Ct=MatrixNormal.transpose()
+    print 'Matriz Ct'
+    print Ct
+    while (Delta>0.0000001):
+        t2=np.dot(Ct,t)
+        t2=np.dot((1-a),t2)+np.dot(a,p)
+        RestaVectores=t2-t
+        print 'resta de vectores =',RestaVectores
+        Delta=LA.norm(RestaVectores)
+        #print'Delta=',Delta
+        t=t2
+
+    print 'Ultimo Delta',Delta
+    print 'Vector t2 final suma: ', t2.sum(axis=0)
+    print 'Vector de Confianza Final :',t2
 
 
 
